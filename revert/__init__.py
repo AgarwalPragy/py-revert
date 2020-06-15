d1c3b74511972ed0db9a9974a775fa6cbe2b3f83
Pragy Agarwal <agar.pragy@gmail.com>
2020-06-16 02:58:46 +0530

    bump version

--- revert/__init__.py
+++ revert/__init__.py
@@ -6,7 +6,7 @@ from .revert import *
 
 __author__ = """Pragy Agarwal"""
 __email__ = 'agar.pragy@gmail.com'
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 
 # noinspection PyUnresolvedReferences
 from . import ogm
--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AgarwalPragy/py-revert',
-    version='0.4.3',
+    version='0.4.4',
     zip_safe=False,
 )
